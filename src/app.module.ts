8ebe00271cbe35322cdf0aad85b8e2838e03bacc
Iago Nobre <iagonobre22@gmail.com>
2026-04-04 23:47:20 -0300

    fix: missing nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { EmpresaModule } from './empresa/empresa.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, EmpresaModule, AuthModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, EmpresaModule, AuthModule],
   controllers: [],
   providers: [],
 })
