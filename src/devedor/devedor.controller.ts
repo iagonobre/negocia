ba76cab6ccc799407a074368ded61ac0395958a0
nathamfernandes <natham.fr@gmail.com>
2026-04-06 08:45:39 -0300

    feat: implementação de create de devedor

--- src/devedor/devedor.controller.ts
+++ src/devedor/devedor.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DevedorService } from './devedor.service';
+import { CreateDevedorDto } from './dto/create-devedor.dto';
 
 @ApiTags('Devedor')
 @Controller('devedor')
 export class DevedorController {
-    constructor (private readonly devedorServiec: DevedorService) {}
+    constructor (private readonly devedorService: DevedorService) {}
 
-    // @Post('cadastrar')
-    // @ApiOperation({ summary: 'Cadastro de novo cliente' })
-    //   @ApiBody({ type: CreateEmpresaDto })
-    //   async cadastrar(@Body() dto: CreateEmpresaDto) {
-    //     return this.empresaService.cadastrar(dto);
-    // }
+    @Post('cadastrar')
+    @ApiOperation({ summary: 'Cadastro de novo devedor' })
+    @ApiBody({ type: CreateDevedorDto })
+    async cadastrar(@Body() dto: CreateDevedorDto) {
+      return this.devedorService.cadastrar(dto);
+    }
 }
